2b122eb8e6aae0f8f384798e906eb0427ddfa6d2
Kittipong Kittirungcharoen (IT) <kitkitti@deltaww.com>
2026-09-18 10:47:54 +0700

    Fix deployment bug: middleware.ts is inside incorrect folder.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 // middleware.ts
 import { createServerClient } from '@supabase/ssr';
-import { NextResponse, type NextRequest } from 'next/request';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
